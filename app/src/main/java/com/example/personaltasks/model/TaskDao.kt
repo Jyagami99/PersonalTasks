8e2049a31349f1bf15c4c1a87eba1ef1d81a7b27
João Marcelo Simão de Castro <jm.castro@usp.br>
2025-05-17 13:05:48 -0300

    refactor: DAO to suport corotine

--- app/src/main/java/com/example/personaltasks/model/TaskDao.kt
+++ app/src/main/java/com/example/personaltasks/model/TaskDao.kt
@@ -1,17 +1,19 @@
 package com.example.personaltasks.model
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 
+@Dao
 interface TaskDao {
     @Insert
-    fun createTask(task: Task): Long
+    suspend fun createTask(task: Task): Long
     @Update
-    fun updateTask(task: Task): Int
+    suspend fun updateTask(task: Task): Int
     @Delete
-    fun deleteTask(task: Task): Int
+    suspend fun deleteTask(task: Task): Int
     @Query(value = "SELECT * FROM Task WHERE id = :id")
     fun retrieveTask(id: Int): Task
     @Query(value = "SELECT * FROM Task")
